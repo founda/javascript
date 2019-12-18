f3baa9173fa6c379244b6da9c89a4cfd206266bf
Gabri <gabriele@founda.com>
2019-12-18 16:43:29 +0100

    update license

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,14 +1,4 @@
 /*
- * Copyright (c) 2019 Founda - All Rights Reserved.
- *
- * Unauthorized copying of this file via any medium IS STRICTLY PROHIBITED.
- * Proprietary and confidential.
- *
- * The above copyright notice shall be included in all copies or
- * substantial portions of the Software.
- */
-
-/*
  * Module dependencies
  */
 
--- README.md
+++ README.md
@@ -3,7 +3,4 @@
 # Cross project code style
 
 > Based on [Airbnb code style](https://github.com/airbnb/javascript)
-
-## License
-
-Copyright (c) 2019 Founda - All Rights Reserved.
+> MIT License [Airbnb](https://github.com/airbnb/javascript/blob/master/LICENSE.md)
